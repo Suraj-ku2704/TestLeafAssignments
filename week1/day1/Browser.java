6f3f2c6c73252482c98c5c77bb30bcc1e0dd674b
Suraj-ku2704 <128149671+Suraj-ku2704@users.noreply.github.com>
2023-10-14 21:23:48 +0530

    Add files via upload
    Week1.Day1 Assignments

---
+++ week1/day1/Browser.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 12.01231f;
+		String browserName = "Chrome";
+		boolean pageLoaded = true;
+		int noOfBrowser = 2;
+		char browserLogo = 'C';
+		
+		System.out.println("Browser Version is"+" "+browserVersion);
+		System.out.println("Browser Name is"+" "+browserName);
+		System.out.println("Page is loaded:"+" "+ pageLoaded);
+		System.out.println("No. Of Browser:"+" "+ noOfBrowser);
+		System.out.println("Browser Logo is"+" "+ browserLogo);
+		
+	}
+}
